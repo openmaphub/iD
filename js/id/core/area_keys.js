fa3ea9de6f6ced9c1c284aaa87e6b12fc6008f29
Sajjad Anwar <me@sajjad.in>
2014-02-19 00:02:56 +0530

    Removing non-moabi presets

--- js/id/core/area_keys.js
+++ js/id/core/area_keys.js
@@ -1,84 +1,17 @@
 /* jshint -W109 */
 iD.areaKeys = {
-    "aeroway": {
-        "gate": true,
-        "taxiway": true
-    },
-    "amenity": {
-        "atm": true,
-        "bench": true,
-        "clock": true,
-        "drinking_water": true,
-        "post_box": true,
-        "telephone": true,
-        "vending_machine": true,
-        "waste_basket": true
-    },
     "area": {},
-    "barrier": {
-        "block": true,
-        "bollard": true,
-        "cattle_grid": true,
-        "cycle_barrier": true,
-        "entrance": true,
-        "gate": true,
-        "kissing_gate": true,
-        "lift_gate": true,
-        "stile": true,
-        "toll_booth": true
-    },
-    "building": {
-        "entrance": true
-    },
-    "craft": {},
-    "emergency": {
-        "fire_hydrant": true,
-        "phone": true
-    },
-    "golf": {
-        "hole": true
-    },
-    "historic": {
-        "boundary_stone": true
-    },
     "boundary": {},
     "landuse": {},
-    "leisure": {
-        "slipway": true
-    },
-    "man_made": {
-        "cutline": true,
-        "embankment": true,
-        "flagpole": true,
-        "pipeline": true,
-        "survey_point": true
-    },
-    "military": {},
-    "natural": {
-        "coastline": true,
-        "peak": true,
-        "spring": true,
-        "tree": true
-    },
-    "office": {},
-    "piste:type": {},
-    "place": {},
     "power": {
         "line": true,
-        "minor_line": true,
-        "pole": true,
-        "tower": true
+        "minor_line": true
     },
     "public_transport": {
         "stop_position": true
     },
     "protected_area": {},
-    "shop": {},
-    "tourism": {
-        "viewpoint": true
-    },
     "waterway": {
-        "canal": true,
         "ditch": true,
         "drain": true,
         "river": true,
