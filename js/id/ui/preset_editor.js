996eb8d10adb3c050f14b0d7d2ae0d6d344802fa
Sajjad Anwar <me@sajjad.in>
2014-04-01 17:36:12 +0530

    restructure functions

--- js/id/ui/preset_editor.js
+++ js/id/ui/preset_editor.js
@@ -7,35 +7,35 @@ iD.ui.PresetEditor = function(context) {
         selection.html('');
 
         var header = selection.append('div')
-            .attr('class', 'header fillL');
+        .attr('class', 'header fillL');
 
         header.append('button')
-            .attr('class', 'fr')
-            .on('click', function() {context.enter(iD.modes.Browse(context));})
-            .append('span')
-            .attr('class', 'icon close');
+        .attr('class', 'fr')
+        .on('click', function() {context.enter(iD.modes.Browse(context));})
+        .append('span')
+        .attr('class', 'icon close');
 
         header.append('h3')
-            .text('Preset Editor');
+        .text('Preset Editor');
 
         var body = selection.append('div')
-            .attr('class', 'body');
+        .attr('class', 'body');
 
 
 
         var addNewButton = body.append('modal-section')
-            .append('button')
-            .attr('class', 'action col4 button preset-editor')
-            .on('click', renderPresetForm)
-            .text('Add New Preset');
+        .append('button')
+        .attr('class', 'action col4 button preset-editor')
+        .on('click', function () { renderPresetForm(body); } )
+        .text('Add New Preset');
 
         var editExistingPointPresetButton = body.append('modal-section')
-            .append('button')
-            .attr('class', 'action col4 button preset-editor')
-            .on('click', function() {
-                editExistingPresets();
-            })
-            .text('Edit Existing Preset');
+        .append('button')
+        .attr('class', 'action col4 button preset-editor')
+        .on('click', function() {
+            editExistingPresets();
+        })
+        .text('Edit Existing Preset');
 
         function editExistingPresets() {
             geometryType = 'point';
@@ -44,106 +44,108 @@ iD.ui.PresetEditor = function(context) {
             console.log("presetList", editPresetList);
             body.attr('class', 'preset-editor');
             body.call(editPresetList);
+        }                
+
+    }
+
+    function renderPresetForm (body) {
+
+        body.html('');
+
+        var noticeSection = body.append('div')
+        .attr('class', 'modal-section warning-section fillL2')
+        .style('display', 'none')
+        .append('h3');
+
+        var presetSection = body.append('div')
+        .attr('class', 'modal-section');
+
+        var presetForm = presetSection.append('div')
+        .attr('class', 'preset-form fillL3');
+
+        var presetFormField = presetForm.append('div')
+        .attr('class', 'form-field form-field-name');
+
+        var presetNameLabel = presetFormField.append('label')
+        .attr('class', 'form-label')
+        .text('Preset Name');
+
+        var presetNameForm = presetFormField.append('input')
+        .attr('id', 'preset-input-name')
+        .attr('style', 'width: 100%;');
+
+        var tagSection = body.append('div')
+        .attr('class', 'modal-section');
+
+        var presetEditor = tagSection.append('div')
+        .attr('class', 'preset-editor')
+
+        var tagList = presetEditor.append('div')
+        .append('ul')
+        .attr('class', 'tag-list')
+        .call(rawTagRow);
+
+        var addTagButton = presetEditor.append('button')
+        .on('click', rawTagRow)
+        .attr('class', 'add-tag')
+        .append('span')
+        .attr('class', 'icon plus light');
+
+        var saveButton = body.append('modal-section')
+        .append('button')
+        .attr('class', 'action col4 button preset-editor')
+        .on('click', applyPresets)
+        .text('Save');
+    }
+
+
+
+    function rawTagRow () {
+
+        $selection = d3.select('.sidebar-component .tag-list');
+
+        $row = $selection.append('li')
+        .attr('class', 'tag-row cf');
+
+        $key = $row.append('div')
+        .attr('class', 'key-wrap')
+        .append('input')
+        .property('type', 'text')
+        .attr('class', 'key')
+        .attr('maxlength', 255)
+        .on('input', inputevent)
+        .on('keydown', keydown);
+
+        $row.append('div')
+        .attr('class', 'input-wrap-position')
+        .append('input')
+        .property('type', 'text')
+        .property('disabled', true)
+        .attr('class', 'value')
+        .attr('maxlength', 255);
+
+        $row.append('button')
+        .attr('tabindex', -1)
+        .attr('class', 'remove minor')
+        .on('click', removeTag)
+        .append('span')
+        .attr('class', 'icon delete');
+
+        $row.append('div')
+        .attr('class', 'tag-reference-body cf');
+    }
+
+    function inputevent () {
+        row = d3.select(this.parentNode.parentNode);
+        row.select('input.value').property('disabled', false);
+    }
+
+    function keydown () {
+        row = d3.select(this.parentNode.parentNode);
+        if (d3.select(this).property('value').length == 0) {
+            row.select('input.value').property('disabled', true);
         }
-
-        function renderPresetForm () {
-
-            body.html('');
-
-            var noticeSection = body.append('div')
-                .attr('class', 'modal-section warning-section fillL2')
-                .style('display', 'none')
-                .append('h3');
-
-            var presetSection = body.append('div')
-                .attr('class', 'modal-section');
-
-            var presetForm = presetSection.append('div')
-                .attr('class', 'preset-form fillL3');
-
-            var presetFormField = presetForm.append('div')
-                .attr('class', 'form-field form-field-name');
-
-            var presetNameLabel = presetFormField.append('label')
-                .attr('class', 'form-label')
-                .text('Preset Name');
-
-            var presetNameForm = presetFormField.append('input')
-                .attr('id', 'preset-input-name')
-                .attr('style', 'width: 100%;');
-
-            var tagSection = body.append('div')
-                .attr('class', 'modal-section');
-
-            var presetEditor = tagSection.append('div')
-                .attr('class', 'preset-editor')
-
-            var tagList = presetEditor.append('div')
-                .append('ul')
-                .attr('class', 'tag-list')
-                .call(rawTagRow);
-
-            var addTagButton = presetEditor.append('button')
-                .on('click', rawTagRow)
-                .attr('class', 'add-tag')
-                .append('span')
-                .attr('class', 'icon plus light');
-
-            var saveButton = body.append('modal-section')
-                .append('button')
-                .attr('class', 'action col4 button preset-editor')
-                .on('click', applyPresets)
-                .text('Save');
-        }
-
-
-
-        function rawTagRow () {
-
-            $selection = d3.select('.sidebar-component .tag-list');
-
-            $row = $selection.append('li')
-            .attr('class', 'tag-row cf');
-
-            $key = $row.append('div')
-            .attr('class', 'key-wrap')
-            .append('input')
-            .property('type', 'text')
-            .attr('class', 'key')
-            .attr('maxlength', 255)
-            .on('input', inputevent)
-            .on('keydown', keydown);
-
-            $row.append('div')
-            .attr('class', 'input-wrap-position')
-            .append('input')
-            .property('type', 'text')
-            .property('disabled', true)
-            .attr('class', 'value')
-            .attr('maxlength', 255);
-
-            $row.append('button')
-            .attr('tabindex', -1)
-            .attr('class', 'remove minor')
-            .on('click', removeTag)
-            .append('span')
-            .attr('class', 'icon delete');
-
-            $row.append('div')
-            .attr('class', 'tag-reference-body cf');
-        }
-
-        function inputevent () {
-            row = d3.select(this.parentNode.parentNode);
-            row.select('input.value').property('disabled', false);
-        }
-
-        function keydown () {
-            row = d3.select(this.parentNode.parentNode);
-            if (d3.select(this).property('value').length == 0) {
-                row.select('input.value').property('disabled', true);
-            }
-        }
+    }
 
         // $button = $inspectorBody.append('button')
         // .attr('class', 'action col4 button preset-editor')
@@ -161,9 +163,9 @@ iD.ui.PresetEditor = function(context) {
         
         function applyPresets () {
             var tags = {},
-                geometry = ["point", "area"],
-                fields = {},
-                name = {};
+            geometry = ["point", "area"],
+            fields = {},
+            name = {};
 
             var presetName = d3.select('#preset-input-name').value();
             
@@ -173,17 +175,17 @@ iD.ui.PresetEditor = function(context) {
                 key = row.selectAll('input.key').value(),
                 value = row.selectAll('input.value').value();
                 tags[key] = value;
-                });
+            });
             iD.data.presets.presets[presetName] = {'tags': tags, 'geometry': geometry, 'name': presetName};
 
             if (validateTags(tags)) {
                 d3.select('.warning-section').style('display', 'block')
-                    .text('New Preset Saved');
+                .text('New Preset Saved');
                 context.presets().load(iD.data.presets);
             }
             else {
                 d3.select('.warning-section').style('display', 'block')
-                    .text('Tags must have a value.');
+                .text('Tags must have a value.');
             }
         }
 
@@ -197,9 +199,8 @@ iD.ui.PresetEditor = function(context) {
                     return true;
                 }
             }
-        }                
+        }
 
+
+        return d3.rebind(presetEditor, event, 'on');
     }
-
-    return d3.rebind(presetEditor, event, 'on');
-}
