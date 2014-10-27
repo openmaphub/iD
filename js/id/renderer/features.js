45cb7488204dd31e71ca1fa776eee034de9aa3ec
Sajjad Anwar <me@sajjad.in>
2014-10-27 14:26:46 -0400

    Add a filter fir a feature that's referenced in the url, work in progress

--- js/id/renderer/features.js
+++ js/id/renderer/features.js
@@ -154,6 +154,19 @@ iD.Features = function(context) {
         }, true);
     });
 
+    // Check if a way is in the hash.
+    var hash = window.location.hash;
+    var featureID = iD.util.stringQs(hash.substring(1)).id;
+    if (featureID) {
+        _.forEach(_.omit(feature, 'selection'), function (f) {
+            f.disable();
+        });
+        defineFeature('selection', function(entity) {
+            if (entity.id === featureID) {
+                return entity;
+            }
+        });
+    }
 
     function features() {}
 
