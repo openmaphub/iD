30d32e6cca8b6c5c2c0526ecd48426910b496e41
Sajjad Anwar <me@sajjad.in>
2014-10-29 17:20:12 -0400

    Remove shared variables between tests and use 'interesting' node to test drawing

--- test/spec/svg/vertices.js
+++ test/spec/svg/vertices.js
@@ -1,18 +1,9 @@
 describe("iD.svg.Vertices", function () {
     var surface,
         projection = Object,
-        node,
-        way1,
-        way2,
-        graph,
         context;
 
     beforeEach(function () {
-        node = iD.Node({loc: [0, 0]});
-        way1 = iD.Way({nodes: [node.id], tags: {highway: 'residential'}});
-        way2 = iD.Way({nodes: [node.id], tags: {highway: 'residential'}});
-        graph = iD.Graph([node, way1, way2]);
-
         context = iD();
         context.map().zoom(context.minEditableZoom());
         surface = d3.select(document.createElementNS('http://www.w3.org/2000/svg', 'svg'))
@@ -20,13 +11,21 @@ describe("iD.svg.Vertices", function () {
     });
 
     it("adds the .shared class to vertices that are members of two or more ways", function () {
+        var node = iD.Node({loc: [0, 0]}),
+            way1 = iD.Way({nodes: [node.id], tags: {highway: 'residential'}}),
+            way2 = iD.Way({nodes: [node.id], tags: {highway: 'residential'}}),
+            graph = iD.Graph([node, way1, way2]);
+
         surface.call(iD.svg.Vertices(projection, context), graph, [node], 17);
         expect(surface.select('.vertex')).to.be.classed('shared');
     });
 
     it("no vertices are drawn if map is not editable", function () {
-        context.map().zoom(context.minEditableZoom() - 1);
+        var node = iD.Node({loc: [0, 0], tags: {amenity: 'atm'}}),
+            way = iD.Way({nodes: [node.id], tags: {highway: 'residential'}}),
+            graph = iD.Graph([node, way]);
 
+        context.map().zoom(context.minEditableZoom() - 1);
         surface.call(iD.svg.Vertices(projection, context), graph, [node], 17);
         expect(surface.select('.vertex')).to.be.classed('shared');
     });
